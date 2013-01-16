4feed6a0b382c0455489ceb1082449ccefd758c9
tommorte <tomas@finn.no>
2013-01-16 08:14:46 +0100

    maven archetype generate

---
+++ src/main/java/no/finntech/kundefront/kata/App.java
@@ -0,0 +1,13 @@
+package no.finntech.kundefront.kata;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
