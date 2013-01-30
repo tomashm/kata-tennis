68470cbd664219f71ee9b405a86aed4d6f111b91
tommorte <tomas@finn.no>
2013-01-30 12:22:18 +0100

    First test Green!

--- src/main/java/no/finntech/kundefront/kata/Game.java
+++ src/main/java/no/finntech/kundefront/kata/Game.java
@@ -2,6 +2,6 @@ package no.finntech.kundefront.kata;
 
 public class Game {
     public Score getScore() {
-        return null;
+        return new Score(0,0);
     }
 }
--- src/main/java/no/finntech/kundefront/kata/Score.java
+++ src/main/java/no/finntech/kundefront/kata/Score.java
@@ -9,4 +9,24 @@ public class Score {
         this.server = server;
         this.receiver = receiver;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Score score = (Score) o;
+
+        if (receiver != score.receiver) return false;
+        if (server != score.server) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = server;
+        result = 31 * result + receiver;
+        return result;
+    }
 }
