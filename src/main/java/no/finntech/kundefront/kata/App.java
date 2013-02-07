1eddbe291a32096d857d28bfbd3ab0955a9e5f3a
tommorte <tomas@finn.no>
2013-02-07 10:55:42 +0100

    Created simple GUI

--- src/main/java/no/finntech/kundefront/kata/App.java
+++ src/main/java/no/finntech/kundefront/kata/App.java
@@ -1,10 +1,29 @@
 package no.finntech.kundefront.kata;
 
+import javax.swing.*;
+
 /**
  * Hello world!
  */
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Game game = new Game();
+        while (true) {
+            String choice = JOptionPane.showInputDialog("Ball won by [s]erver or [r]eceiver");
+            if (choice == null) return;
+            switch (choice) {
+                case "s":
+                    game.registerBallWinner(Player.SERVER);
+                    break;
+                case "r":
+                    game.registerBallWinner(Player.RECEIVER);
+                    break;
+                default:
+                    continue;
+            }
+            JOptionPane.showMessageDialog(null, game.getScore());
+
+            if (game.isDone()) game = new Game();
+        }
     }
 }
